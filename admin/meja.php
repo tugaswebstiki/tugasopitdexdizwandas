6476728542fba45add42d0757b868dde8c45216a
opitpratama <opitopit33@gmail.com>
2018-06-21 20:00:11 +0800

    restorant

--- admin/meja.php
+++ admin/meja.php
@@ -1,8 +1,12 @@
 <div>
 
-	<div><h2>DAFTAR MEJA</h2><hr> 
-		<a href="index.php?halaman=tambahmeja" class="btn btn-success">Tambah</a> 
+	<div><h2>DAFTAR MEJA</h2><hr></div>
+	<div class="lebar80">
+		<a href="#tambahmeja" data-toggle="modal" class="btn btn-success">Tambah</a> 
+		<a href="#hapusmeja" data-toggle="modal" class="btn btn-danger floatright">Hapus</a> 
+		
 	</div>
+
 	<br>
 
 	
@@ -57,5 +61,132 @@
 								
 					<?php } ?>
 
+	<!--   
+===============================================TAMBAH MEJA======================================================
+-->
 
+<div class="modal fade" id="tambahmeja" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-sm" role="document"> 
+		<div class="modal-content">
+			<div class="modal-body">
+
+
+
+				<div class="modal-header">
+					<h2>TAMBAH MEJA</h2>
+				</div>
+
+				<form method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label>No Meja</label>
+		<input type="text" class="form-control" name="nama">
+	</div>
+	
+	<div class="form-group">
+		<label>Jumlah Kursi</label>
+		<input type="text" class="form-control" name="jk">
+	</div>
+<hr>
+	<button class="btn btn-success" name="save">Simpan</button>
+
+</form>
+
+		<?php 
+
+		if (isset($_POST['save'])) {
+
+
+
+			$koneksi->query("	INSERT INTO meja
+								(nama_meja,jumlah_kursi,id_status) 
+								VALUES('$_POST[nama]','$_POST[jk]',2);
+							");
+			
+			echo "<div class='alert-info alert'> Data Tersimpan </div>";
+			echo "<meta http-equiv='refresh' content='1;url=index.php'>";		
+			}
+			
+		 ?>
+
+
+
+						</div>	
+					</div>	
+				</div>
+			</div>
+<!--   
+================================================TAMBAH MEMBER======================================================
+-->
+</div>
+	<!--   
+===============================================HAPUS MEJA======================================================
+-->
+
+<div class="modal fade" id="hapusmeja" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-sm" role="document"> 
+		<div class="modal-content">
+			<div class="modal-body">
+
+
+
+				<div class="modal-header">
+					<h2>HAPUS MEJA</h2>
+				</div>
+
+				<form method="post" enctype="multipart/form-data">
+	<div class="form-group">
+						<label>Meja yang ingin dihapus</label>
+						<?php   $ambil = $koneksi->query(	"SELECT * 
+							FROM meja");
+							?>
+							<select class="form-control" name="kategori">
+
+								<option>Pilih Meja</option>
+
+								<?php
+								while($pecah = $ambil->fetch_assoc()){ 		?>
+							
+									<option value="<?php echo $pecah['id_meja']; ?>">
+									
+										<?php echo $pecah['nama_meja']; ?></option>
+
+									<?php  } ?>
+
+
+								</select>
+							</div>
+	
+	
+<hr>
+	<button class="btn btn-success" name="delete">Hapus</button>
+
+</form>
+
+		<?php 
+
+		if (isset($_POST['delete'])) {
+
+			$cari = $koneksi->query("SELECT id_status FROM meja WHERE id_meja='$_POST[kategori]'");
+
+
+			if ($cari=2) {
+			$koneksi->query("DELETE FROM meja WHERE id_meja='$_POST[kategori]'");
+						
+						echo "<div class='alert-info alert'> Data Tersimpan </div>";
+						echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+			}
+			else{	
+			}
+			}
+		 ?>
+
+
+
+						</div>	
+					</div>	
+				</div>
+			</div>
+<!--   
+================================================HAPUS MEMBER======================================================
+-->
 </div>
